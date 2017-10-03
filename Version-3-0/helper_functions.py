32d4a8eff4cd528525d0afd67f3ca68644b76dfa
Radu Darie <radu_darie@brown.edu>
2017-10-02 21:43:10 -0400

    get_kinematics

--- Version-3-0/helper_functions.py
+++ Version-3-0/helper_functions.py
@@ -3,16 +3,31 @@ import numpy as np
 import seaborn as sns
 import itertools
 
-def get_kinematics(filePath):
-    raw = pd.read_table(filePath, index_col = 0, skiprows = [1])
-
-    headings = list(raw)
-    uniqueHeadings = set([name[:-2] for name in headings])
+def get_kinematics(kinematicsFile, selectHeaders = None, selectTime = None):
+    raw = pd.read_table(kinematicsFile, index_col = 0, skiprows = [1])
+    if selectTime:
+        raw = raw.loc[slice(selectTime[0], selectTime[1]), :]
+    headings = sorted(raw.columns) # get column names
     coordinates = ['x', 'y', 'z']
 
-    indexPD = pd.MultiIndex.from_product([sorted(uniqueHeadings), coordinates], names=['joint', 'coordinate'])
-    proc = pd.DataFrame(index = indexPD).T
+    # reorder alphabetically by columns
+    raw = raw.reindex_axis(headings, axis = 1)
 
-    for idx, row in raw.iterrows():
-        for col, value in row.iteritems():
-            proc.loc[idx, (col[:-2], col[-1].lower())] = value
+    # create multiIndex column names
+    if selectHeaders is not None:
+        expandedHeadings = [
+            name + ' X' for name in selectHeaders
+        ] + [
+            name + ' Y' for name in selectHeaders
+        ] + [
+            name + ' Z' for name in selectHeaders
+        ]
+        raw = raw[expandedHeadings]
+        indexPD = pd.MultiIndex.from_product([sorted(selectHeaders), coordinates], names=['joint', 'coordinate'])
+    else:
+        uniqueHeadings = set([name[:-2] for name in headings])
+        indexPD = pd.MultiIndex.from_product([sorted(uniqueHeadings), coordinates], names=['joint', 'coordinate'])
+
+    proc = pd.DataFrame(raw.values, columns = indexPD, index = raw.index)
+
+    return proc
--- Version-3-0/setting_state.py
+++ Version-3-0/setting_state.py
@@ -3,7 +3,7 @@
 Example for how to modifying the MuJoCo qpos during execution.
 """
 
-import os, pdb
+import os, pdb, argparse
 from mujoco_py import load_model_from_xml, MjSim, MjViewer
 from helper_functions import *
 
@@ -14,20 +14,26 @@ curDir = os.path.abspath(os.path.join(curfilePath,os.pardir)) # this will return
 parentDir = os.path.abspath(os.path.join(curDir,os.pardir)) # this will return parent directory.
 #print(parentDir)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--kinematicsFile', default = 'W:/ENG_Neuromotion_Shared/group/Proprioprosthetics/Training/T_1.txt')
+
+args = parser.parse_args()
+
+kinematicsFile = args.kinematicsFile
+
 with open(curDir + '/murdoc.xml', 'r') as f:
     MODEL_XML = f.read()
 
 MODEL_XML = MODEL_XML.replace('$MURDOC_DIR$', curDir + '/Resources/Murdoc')
 
-def print_box_xpos(sim):
-    print("box xpos:", sim.data.get_body_xpos("box"))
-
 model = load_model_from_xml(MODEL_XML)
 sim = MjSim(model)
-viewer = MjViewer(sim)
+#viewer = MjViewer(sim)
 
 #Get kinematics
-sitenames, target = get_kin('TRM20', trials)
+kin = get_kinematics(kinematicsFile,
+    selectHeaders = ['MT_Left', 'M_Left', 'C_Left', 'GT_Left', 'K_Left'],
+    selectTime = [26, 50])
 
 states = [{'box:x': +0.8, 'box:y': +0.8},
           {'box:x': -0.8, 'box:y': +0.8},
@@ -38,21 +44,17 @@ states = [{'box:x': +0.8, 'box:y': +0.8},
 # MjModel.joint_name2id returns the index of a joint in
 # MjData.qpos.
 
-#x_joint_i = sim.model.get_joint_qpos_addr("box:x")
+x_joint_i = sim.model.get_joint_qpos_addr("left_knee")
 #y_joint_i = sim.model.get_joint_qpos_addr("box:y")
 
 #print_box_xpos(sim)
 
-while True:
-    for state in states:
-        sim_state = sim.get_state()
-        #sim_state.qpos[x_joint_i] = state["box:x"]
-        #sim_state.qpos[y_joint_i] = state["box:y"]
-        #sim.set_state(sim_state)
-        sim.forward()
-        print("updated state to", state)
-        #print_box_xpos(sim)
-        viewer.render()
-
-    if os.getenv('TESTING') is not None:
-        break
+for idx, row in kin.iterrows():
+    sim_state = sim.get_state()
+    #sim_state.qpos[x_joint_i] = state["box:x"]
+    #sim_state.qpos[y_joint_i] = state["box:y"]
+    #sim.set_state(sim_state)
+    sim.forward()
+    print("updated state to", state)
+    #print_box_xpos(sim)
+    #viewer.render()
