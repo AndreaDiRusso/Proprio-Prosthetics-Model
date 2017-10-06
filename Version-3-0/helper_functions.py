b28e2db251de932733bc0f4a77b0a45fea3510c6
Radu Darie <radu_darie@brown.edu>
2017-10-05 23:32:02 -0400

    programatic description

--- Version-3-0/helper_functions.py
+++ Version-3-0/helper_functions.py
@@ -31,3 +31,37 @@ def get_kinematics(kinematicsFile, selectHeaders = None, selectTime = None):
     proc = pd.DataFrame(raw.values, columns = indexPD, index = raw.index)
 
     return proc
+
+def fscv_to_spec(fcsvFilePath):
+    fcsv = pd.read_csv(fcsvFilePath, skiprows = 2)
+    fcsv = fcsv.loc[:, ['label', 'x', 'y', 'z']]
+    return fcsv
+
+def populate_model(templateFilePath, specification):
+    fcsv = fscv_to_spec(fcsvFilePath)
+
+    with open(templateFilePath, 'r') as f:
+        modelXML = f.read()
+
+    modelXML = modelXML.replace('$Murdoc_Dir$', curDir + '/Resources/Murdoc')
+    for idx, row in fcsv.iterrows():
+
+        if ":x" in row['label'] or ":y" in row['label'] or ':z' in row['label']:
+            jointAxis = row['label'].split(':')[-1][0]
+            jointName = row['label'].split(':')[0]
+            originName = jointName + ':o'
+            row['x'] = - row['x'] + fcsv[fcsv['label'] == originName]['x'].values[0]
+            row['y'] = - row['y'] + fcsv[fcsv['label'] == originName]['y'].values[0]
+            row['z'] = - row['z'] + fcsv[fcsv['label'] == originName]['z'].values[0]
+
+        placeHolder = '$' + row['label'] + ':' + 'x$'
+        modelXML = modelXML.replace(placeHolder, str(row['x']*1e-3))
+
+        placeHolder = '$' + row['label'] + ':' + 'y$'
+        modelXML = modelXML.replace(placeHolder, str(row['y']*1e-3))
+
+        placeHolder = '$' + row['label'] + ':' + 'z$'
+        modelXML = modelXML.replace(placeHolder, str(row['z']*1e-3))
+
+    with open(curDir + '/murdoc_gen.xml', 'w') as f:
+        f.write(modelXML)
